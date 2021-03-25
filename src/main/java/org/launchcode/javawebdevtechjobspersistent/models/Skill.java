3a5ab3a2be64f9223072c01baa51b41a1c59350a
Dathan Rivera <dwrivera7@gmail.com>
2021-03-25 18:42:32 -0500

    First commit, part 2: data layer completed.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String skillDescription;
+
+    public Skill() {};
+
+    public String getSkillDescription() {
+        return skillDescription;
+    }
+
+    public void setSkillDescription(String skillDescription) {
+        this.skillDescription = skillDescription;
+    }
 }
---
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/SkillRepository.java
@@ -0,0 +1,6 @@
+package org.launchcode.javawebdevtechjobspersistent.models.data;
+
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SkillRepository extends CrudRepository<Skill, Integer> {}
